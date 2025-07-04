775f44a3378cabc8800c42daca3dca1dcd243c47
momo-0826 <coco.bass.0826@gmail.com>
2025-07-04 17:12:04 +0900

    新規登録画面の見た目修正

--- frontend/src/features/auth/components/SignUpForm.tsx
+++ frontend/src/features/auth/components/SignUpForm.tsx
@@ -1,6 +1,17 @@
 "use client";
 
-import { Button, TextField, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  // Card,
+  CardContent,
+  Container,
+  TextField,
+  Typography,
+  Stack,
+  Paper,
+} from "@mui/material";
+import { grey, orange } from "@mui/material/colors";
 import { FormProvider, UseFormReturn } from "react-hook-form";
 
 import type { SignUpSchema } from "@/features/auth/types";
@@ -11,30 +22,98 @@ type Props = {
 };
 
 export const SignUpForm = ({ methods, onSubmit }: Props) => {
-  // const methods = useForm<SignUpSchema>()
-
   return (
-    <FormProvider {...methods}>
-      <form onSubmit={methods.handleSubmit(onSubmit)}>
-        <Typography variant="h4">Sign Up</Typography>
-        <TextField
-          label="Email"
-          type="email"
-          {...methods.register("email")}
-          error={!!methods.formState.errors.email}
-          helperText={methods.formState.errors.email?.message}
-        />
-        <TextField
-          label="Password"
-          type="password"
-          {...methods.register("password")}
-          error={!!methods.formState.errors.password}
-          helperText={methods.formState.errors.password?.message}
-        />
-        <Button type="submit" variant="contained">
-          Sign Up
-        </Button>
-      </form>
-    </FormProvider>
+    <Container maxWidth="sm">
+      <Box
+        sx={{
+          minHeight: "100vh",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          py: 4,
+        }}
+      >
+        <Paper elevation={3} sx={{ width: "100%", maxWidth: 400 }}>
+          <CardContent sx={{ p: 4 }}>
+            <FormProvider {...methods}>
+              <form onSubmit={methods.handleSubmit(onSubmit)} noValidate>
+                <Stack spacing={3}>
+                  <Typography variant="h4" component="h1" align="center" gutterBottom sx={{ fontWeight: "bold", color: orange[500] }}>
+                    新規登録
+                  </Typography>
+
+                  <Typography variant="body1" align="center" color="text.secondary" sx={{ mb: 2 }}>
+                    アカウントを作成してください
+                  </Typography>
+
+                  <TextField
+                    fullWidth
+                    label="メールアドレス"
+                    type="email"
+                    variant="outlined"
+                    {...methods.register("email")}
+                    error={!!methods.formState.errors.email}
+                    helperText={methods.formState.errors.email?.message}
+                    sx={{
+                      mb: 2,
+                      "& .MuiOutlinedInput-root": {
+                        "&.Mui-focused fieldset": {
+                          borderColor: orange[500],
+                        },
+                      },
+                      "& .MuiInputLabel-root": {
+                        "&.Mui-focused": {
+                          color: orange[500],
+                        },
+                      },
+                    }}
+                  />
+
+                  <TextField
+                    fullWidth
+                    label="パスワード"
+                    type="password"
+                    variant="outlined"
+                    {...methods.register("password")}
+                    error={!!methods.formState.errors.password}
+                    helperText={methods.formState.errors.password?.message}
+                    sx={{
+                      mb: 3,
+                      "& .MuiOutlinedInput-root": {
+                        "&.Mui-focused fieldset": {
+                          borderColor: orange[500],
+                        },
+                      },
+                      "& .MuiInputLabel-root": {
+                        "&.Mui-focused": {
+                          color: orange[500],
+                        },
+                      },
+                    }}
+                  />
+
+                  <Button
+                    type="submit"
+                    variant="contained"
+                    size="large"
+                    fullWidth
+                    sx={{
+                      py: 1.5,
+                      color: grey[800],
+                      fontSize: "1.1rem",
+                      fontWeight: "bold",
+                      borderRadius: 2,
+                      backgroundColor: orange[500],
+                    }}
+                  >
+                    アカウントを作成
+                  </Button>
+                </Stack>
+              </form>
+            </FormProvider>
+          </CardContent>
+        </Paper>
+      </Box>
+    </Container>
   );
 };
